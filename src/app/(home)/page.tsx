fdfecb62c3ef9e189cde10734b102791192d5fb0
Relix18 <relix049@gmail.com>
2025-05-26 14:17:13 +0530

    database setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
   return (
     <div className="flex min-h-screen flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +15,7 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
+        {documents?.map((doc) => <span key={doc._id}>{doc.title}</span>)}
       </div>
     </div>
   );
