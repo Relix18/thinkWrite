09e385a8f04b744ccc3ad528f45ee3fd67c15d54
Relix18 <relix049@gmail.com>
2025-05-28 00:05:56 +0530

    style: editor padding change

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -72,7 +72,7 @@ export const Editor = ({ initialContent }: EditorProps) => {
       attributes: {
         style: `padding-left:${leftMargin}px; padding-right:${rightMargin}px;`,
         class:
-          "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pr-14 pb-10 cursor-text",
+          "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
     extensions: [
